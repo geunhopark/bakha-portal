3c67deb2a751db3778f2b9a44b1fe0599af42b81
ghpark <ghrpark@dtnsm.com>
2020-05-14 15:14:42 +0900

    그레이들 버전 다운, 스프링부트 어플리케이션 추가, controller/dto 테스트

---
+++ src/main/java/com/bakha/portal/web/dto/HelloResponseDto.java
@@ -0,0 +1,12 @@
+package com.bakha.portal.web.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class HelloResponseDto {
+
+    private final String name;
+    private final int amount;
+}
